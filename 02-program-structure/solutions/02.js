d8cf5786824173bb43a3e1d4e074b5e67c4f4d15
Gody <250132528+matecg@users.noreply.github.com>
2026-04-20 11:41:13 -0300

    add chapt 02 exerc 02 solution

---
+++ 02-program-structure/solutions/02.js
@@ -0,0 +1,15 @@
+/**
+ * Solution(s) to Chapter 02 - Problem 2: FizzBuzz
+ */
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        let toPrint = "";
+        if (i % 3 === 0) toPrint += "Fizz";
+        if (i % 5 === 0) toPrint += "Buzz";
+        if (toPrint.length === 0) toPrint = i.toString();
+        console.log(toPrint);
+    }
+}
+
+fizzBuzz();
