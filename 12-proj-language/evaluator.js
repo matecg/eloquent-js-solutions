ade1a5ac69f0c6679a460d9d9e95d309756e042a
Gody <250132528+matecg@users.noreply.github.com>
2026-05-04 06:42:22 -0300

    move run to evaluator.js and add function handling

--- 12-proj-language/evaluator.js
+++ 12-proj-language/evaluator.js
@@ -1,6 +1,14 @@
-const specialForms = Object.create(null);
+import generateGlobalScope from "./globalScope.js";
+import parse from "./parser.js";
 
-export default function evaluate(expression, scope) {
+const specialForms = Object.create(null);
+const topScope = generateGlobalScope();
+
+export default function run(program) {
+    return evaluate(parse(program), Object.create(topScope));
+}
+
+function evaluate(expression, scope) {
     if (expression.type == "value") {
         return expression.value;
     } else if (expression.type == "word") {
@@ -59,4 +67,28 @@ specialForms.define = (args, scope) => {
     let value = evaluate(args[1], scope);
     scope[args[0].name] = value;
     return value;
+}
+
+specialForms.fun = (args, scope) => {
+    if (!args.length) {
+        throw new SyntaxError("Functions must have a body");
+    }
+    const body = args.at(-1);
+    const params = args.slice(0, args.length - 1).map(expr => {
+        if (expr.type != "word") {
+            throw new SyntaxError("Parameter names must be words");
+        }
+        return expr.name;
+    });
+
+    return function(...args) {
+        if (args.length !== params.length) {
+            throw new TypeError("Wrong number of arguments");
+        }
+        const localScope = Object.create(scope);
+        for (let i = 0; i < args.length; i++) {
+            localScope[params[i]] = args[i];
+        }
+        return evaluate(body, localScope);
+    }
 }
--- 12-proj-language/index.js
+++ 12-proj-language/index.js
@@ -1,3 +1,25 @@
-import parse from "./parser.js";
+import run from "./evaluator.js";
 
-console.log(parse("+(a, 10)"));
+
+run(`
+do(define(total, 0),
+   define(count, 1),
+   while(<(count, 11),
+         do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+   print(total))
+`);
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
+
+run(`
+do(define(pow, fun(base, exp,
+     if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+   print(pow(2, 10)))
+`);
+
