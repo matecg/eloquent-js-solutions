50df50173b667ce18f0368d8652c637b0e3830eb
Gody <250132528+matecg@users.noreply.github.com>
2026-05-12 20:25:50 -0300

    move vec, state and level into engine directory

---
+++ 16-proj-game/engine/level.js
@@ -0,0 +1,42 @@
+import Coin from "../actors/coin.js";
+import Lava from "../actors/lava.js";
+import Player from "../actors/player.js";
+import Vec from "./vec.js";
+
+/**
+ * Class responsible for building game levels.
+ */
+export default class Level {
+    /**
+     * @param {string} plan - A level plan string
+     */
+    constructor(plan) {
+        const clearedPlan = plan.trim().split("\n").map(line => [...line]);
+
+        this.height = clearedPlan.length;
+        this.width = clearedPlan[0].length;
+        this.startActors = [];
+        this.rows = clearedPlan.map((row, y) => {
+            return row.map((char, x) => {
+                let type = levelCharacters[char];
+                if (typeof type !== "string") {
+                    const pos = new Vec(x, y);
+                    this.startActors.push(type.create(pos, char));
+                    type = "empty";
+                }
+                return type;
+            })
+        });
+    }
+}
+
+const levelCharacters = {
+    ".": "empty",
+    "#": "wall",
+    "+": "lava",
+    "@": Player,
+    "o": Coin,
+    "=": Lava,
+    "|": Lava,
+    "v": Lava
+};
---
+++ 16-proj-game/engine/vec.js
@@ -0,0 +1,46 @@
+/**
+ * A 2D Vector class.
+ */
+export default class Vec {
+    /**
+     * Creates a new 2D vector instance.
+     * @param {number} x - The x coordinate
+     * @param {number} y - The y coordinate
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the vector's length, starting on the origin.
+     */
+    get length() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+
+    /**
+     * Add two vectors together.
+     * @param {Vec} vec - A vector to add
+     */
+    plus(vec) {
+        return new Vec(this.x + vec.x, this.y + vec.y);
+    }
+
+    /**
+     * Subtract the current vector with the one provided as an argument.
+     * @param {Vec} vec - A vector to subtract
+     */
+    minus(vec) {
+        return new Vec(this.x - vec.x, this.y - vec.y);
+    }
+
+    /**
+     * Multiply the vector x and y coordinates by a given factor.
+     * @param {number} factor - A factor to multiply the vector
+     * @returns {Vec}
+     */
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+}
