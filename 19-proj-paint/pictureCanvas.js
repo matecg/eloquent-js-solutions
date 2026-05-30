06332cd5fe59d21db3ae0adedd3dd7b4ee1c3844
Gody <250132528+matecg@users.noreply.github.com>
2026-05-30 06:18:54 -0300

    add touch method to PictureCanvas to track touchscreen interactions with canvas

--- 19-proj-paint/pictureCanvas.js
+++ 19-proj-paint/pictureCanvas.js
@@ -36,4 +36,27 @@ export default class PictureCanvas {
         };
         this.dom.addEventListener("mousemove", move);
     } 
+
+    touch(startEvent, onDown) {
+        let pos = pointerPosition(startEvent.touches[0], this.dom);
+        const onMove = onDown(pos);
+        startEvent.preventDefault();
+
+        if (!onMove) return;
+        const move = moveEvent => {
+            const newPos = pointerPosition(moveEvent.touches[0],
+                this.dom
+            );
+            if (newPos.x == pos.x && newPos.y == pos.y) return;
+            pos = newPos;
+            onMove(newPos);
+        };
+        const end = () => {
+            this.dom.removeEventListener("touchmove", move);
+            this.dom.removeEventListener("touchend", end);
+        };
+        
+        this.dom.addEventListener("touchmove", move);
+        this.dom.addEventListener("touchend", end);
+    }
 }
