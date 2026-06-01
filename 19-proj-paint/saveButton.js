cbbddb0e3b7ee67606580998c8a6b94ead92f326
Gody <250132528+matecg@users.noreply.github.com>
2026-06-01 19:51:29 -0300

    add Save Button class to download drawings

---
+++ 19-proj-paint/saveButton.js
@@ -0,0 +1,26 @@
+import { drawPicture } from "./utils.js";
+
+export default class SaveButton {
+    constructor(state) {
+        this.picture = state.picture;
+        this.dom = elt("button", {
+            onclick: () => this.save()
+        }, "💾 Save");
+    }
+
+    save() {
+        const canvas = elt("canvas");
+        drawPicture(this.picture, canvas, 1);
+        const link = elt("a", {
+            href: canvas.toDataURL(),
+            download: "pixelart.png"
+        });
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+    
+    syncState(state) {
+        this.picture = state.picture;
+    }
+}
